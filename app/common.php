95027aa3d46d57c1e390fbaa748fbbc6ae03b6c5
zhangxinkai <zhangxinkai@qq.com>
2025-04-24 10:00:58 +0800

    提交

--- app/common.php
+++ app/common.php
@@ -1216,7 +1216,6 @@ if (!function_exists('imageUrl')) {
         if (strpos($img, 'http') !== false) {
             return $img;
         } else {
-//            return 'https://' . $_SERVER['SERVER_NAME'] . $img;
             return request()->domain() . $img;
         }
     }
