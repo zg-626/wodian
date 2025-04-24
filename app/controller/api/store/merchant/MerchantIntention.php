f8901fcd8ba47898c62c8aa26a544be78ea5ba07
zhangxinkai <zhangxinkai@qq.com>
2025-04-24 10:17:09 +0800

    提交

--- app/controller/api/store/merchant/MerchantIntention.php
+++ app/controller/api/store/merchant/MerchantIntention.php
@@ -155,7 +155,7 @@ class MerchantIntention extends BaseController
                 return app('json')->fail($api->getErrorInfo());
             }
         } catch (Exception $e) {
-            return app('json')->fail('File：' . $e->getFile() . " ，Line：" . $e->getLine() . '，Message：' . $e->get);
+            return app('json')->fail('File：' . $e->getFile() . " ，Line：" . $e->getLine() . '，Message：' . $e->getMessage().' '. $e->getData().' '. $e->getCode().' '. $e->getPrevious().' '. $e->getTrace().' '. $e->getTraceAsString());
         }
 
         $save_data['lkl_ec_apply_id'] = $result['ecApplyId'];
