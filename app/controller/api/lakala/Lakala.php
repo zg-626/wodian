98b42d9fbc693f39980146e53ad4666463f942b6
zhouting <jiujiu3178@163.com>
2025-04-25 16:28:12 +0800

    1

--- app/controller/api/lakala/Lakala.php
+++ app/controller/api/lakala/Lakala.php
@@ -58,15 +58,14 @@ class Lakala extends BaseController
      */
     public function lklMerchantApplyNotify()
     {
-        // $param = input('');
-        // Db::name('third_notify')->insert(['title' => '商户进件回调3333333', 'content' => json_encode($param, JSON_UNESCAPED_UNICODE), 'createtime' => time()]);
-        // record_log('时间: ' . date('Y-m-d H:i:s') . ', 商户进件回调3333333: ' . json_encode($param, JSON_UNESCAPED_UNICODE), 'lkl');
-        $param = '{"data":"rmB6Wop7zPouc\/FN+FUSPCgFBLMQSVgGLxTt7KaaCnmb\/d2NidwjiglqNU\/L6GB7U0A8jTyT593B4\/3JPYgKyJpZ651UZw6rqlTMglHG9O54SrKQ3XfWQW8qKNtipTy1B3vsCcy4Zj5XOoPac53q0MtvgRG7WoFqzy1te8QuEZtsduFivYLHGeTfIK\/SNPVmf1LBXXkos5TtvbVz5upHCO4HHkf71krJlOXIfOVDInz9pqJrzu3Mk0zgPP\/2A1lUXIY8HNLkiU3SpGRw5Zz2vjihgoeGfwuDW4ddtGhWiRAyoyZ2fqJsAU2Vk0rXM07rxMDwWiL7x4ty9Vx44ZJXLaVb89utbd72+rj+aPazNWNeEOFS3yYJFg4k04t\/o+BOoYKoBJxoIhGF1T7HGxk13DpICnJgWPo4ZyFJR8nzv3ALoWWlPvk6ZYCQXArqseilK5T6KhMQvrTMIDYgA1leYoXvYF+uZ6b7Y6SZcxKcyzYN1rM4sTC+1foNNhk03TBGiUVa5F5Kqrcc2l\/phl0HcUiUb52EtAzn4a7ixqYlRTW4HyaJeNoHrTjuolm8uGLETFvd93rPz2CLNiFjnv639deBQf96c3++kLdyhGhJ5Z4FGvHnsLD3u7kmyZa8vUP8KSpDlb9jkKJLrdBd8aSbOpyTBcphEij1CMUb55vYZ3k7sGR4uPQo4LwacNZrinxRZcIIbqj8VxOAeDmT5DY+qkDulNycMlPTrCJCccorSPaB2kp51xl85chutAjD\/tkOjRMV0w7e9\/34vawAh7REYIAgSjXkQ\/b5RFgLom+ejwawwXkwjGW4Q1A2PF9C+BcQ7NVRYjf4geyqAy\/tCcYZEoPwK8T8u0kvcZJBAbs1BpUQhkPw15+1SKECysxuFYboetJr4GdKNdcfrNmrs+pE1HWb8lRgQqr7pov5BGjkTnoRqjsKBTInrX3frxJMLEEoVuKfLGln6Cjz17C1WiV\/ApdO8T5M3pM4aIsAn9VFR4zf9\/WDGU5ZB8Bk\/8owEq8w","id":245989226934281}';
+        $param = input('');
+        Db::name('third_notify')->insert(['title' => '商户进件回调', 'content' => json_encode($param, JSON_UNESCAPED_UNICODE), 'createtime' => time()]);
+        record_log('时间: ' . date('Y-m-d H:i:s') . ', 商户进件回调: ' . json_encode($param, JSON_UNESCAPED_UNICODE), 'lkl');
         $resArr = json_decode($param, true);
         echo "<pre>";
         print_r($resArr);
 
-        $pubKey = 'MIIEMTCCAxmgAwIBAgIGAXUrc4b4MA0GCSqGSIb3DQEBCwUAMHYxCzAJBgNVBAYTAkNOMRAwDgYDVQQIDAdCZWlKaW5nMRAwDgYDVQQHDAdCZWlKaW5nMRcwFQYDVQQKDA5MYWthbGEgQ28uLEx0ZDEqMCgGA1UEAwwhTGFrYWxhIE9yZ2FuaXphdGlvbiBWYWxpZGF0aW9uIENBMB4XDTIwMTAxNTA4NDk1MloXDTMwMTAxMzA4NDk1MlowZTELMAkGA1UEBhMCQ04xEDAOBgNVBAgMB0JlaUppbmcxEDAOBgNVBAcMB0JlaUppbmcxFzAVBgNVBAoMDkxha2FsYSBDby4sTHRkMRkwFwYDVQQDDBBBUElHVy5MQUtBTEEuQ09NMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAwAXZw9lupWcFXouCNhm0DQT47Zf4KOIRF8rqT8Ps3pYzT8odROJ8rq4P+lciGrg29czpqrRM22yQktFritvcM7JlE6jFbGH3rycnvGvhRYU/j1N9k0ozm8oVwmKX357/OtGzNivBECGSnU9LBkp4Nm9M1K4cOwEuZ0xsQEthZjQYF0mDpnlWmVJL5i1Lq834atN2qrb/mzMHBNtDJnqRV7rPL39lKpe7LJiitsC2JuW1UbWZZU1NNwA/rz2d83C+KD1DLJ0+sMYY2Q3TOQ4BPAowDEwOH7XAXrHM/0kRm+ZeIFlwevEGIQWmMt1Ogz+AW4Iq0slINc4wOINKvH9tHwIDAQABo4HVMIHSMIGSBgNVHSMEgYowgYeAFCnH4DkZPR6CZxRn/kIqVsModJHpoWekZTBjMQswCQYDVQQGEwJDTjEQMA4GA1UECAwHQmVpSmluZzEQMA4GA1UEBwwHQmVpSmluZzEXMBUGA1UECgwOTGFrYWxhIENvLixMdGQxFzAVBgNVBAMMDkxha2FsYSBSb290IENBggYBaiUALIowHQYDVR0OBBYEFIya0Yc4OSBer55JLyA0AYe9m8mTMAwGA1UdEwEB/wQCMAAwDgYDVR0PAQH/BAQDAgeAMA0GCSqGSIb3DQEBCwUAA4IBAQCBEwOlk3mXigNv94Drn3dcaY2ml/y+8yNpAIuUhuBE00WFoqEX5lOatFy5fzdXuC12lBVQ8SjSm3aH7k2X0eXqDzkOHiur2ZBRKmJ++J4TeenuSUOjSIbQK/DTvxaqFUjYwFSVCyizpy7wfU4wKt+jOuFb9LyULJ9lkM1dV9Kh7Lmd9+nlJYYuPEPULJkkVZqSALSiiJudXnTwlISjZTXEAkJpdIlMw+hvPTAkoG95B95M+OV/uLbItGK+qT4+RHWo8EbBDPQYo6J4QYHOxRlfMoGBMyrz6XDt7ELLmT7ld4aE02w6KQPfK3gqkLDT+/STozvaNmXzBJh7J6KqxJBH';
+        $pubKey = 'MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAtY2D9l3S9gpZWmIUVxLFbCnQ1fxJE1lw90wYGoSgiVRqUk5DBQhqy+7JRyQiFTdp2Xam3BY0+G3YtA27rROaTIDd7hrDWWvdqIstufBAJlhwWfVIusez/HdSb3u2sunhjV6BWenjezUyQSoR4avREqNRzPOBod7QAYOeaLWL8W8edgfTC2/8Pj9W+Dpk5XZ35YG8eOdMXNxApYq2lZu4OuqH80/22DmnIWOZ6GIjcNxPE9Bp7soQjVDCGFW/gjH3t+EYB9HC0/NEd8btajTnrlzueAL6Md7ItDCd4gbBmph3caOTMdqzAjbDuALrGNsGXaNhvrC7CHeazICzG+rSxQIDAQAB';
         $res = self::publicKeyDecrypt($resArr['data'], $pubKey);
         echo "<pre>";
         print_r($res);
