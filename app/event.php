64d70df7d22e89d951e9c25fe6fae346a6e3dd70
lsm <1058474495@qq.com>
2025-05-06 10:23:41 +0800

    feat(master): 去掉分红定时

--- app/event.php
+++ app/event.php
@@ -63,7 +63,7 @@ return [
             \crmeb\listens\SendSvipCouponListen::class,
             \crmeb\listens\SyncMerchantMarginStatusListen::class,
             \crmeb\listens\SyncQueueStatusListen::class,
-            DividendTaskListen::class,
+            //DividendTaskListen::class,// 自动分红
         ] : [],
         'pay_success_user_recharge' => [\crmeb\listens\pay\UserRechargeSuccessListen::class],
         'pay_success_offline_order' => [\crmeb\listens\pay\OfflineOrderSuccessListen::class],
--- crmeb/listens/DividendTaskListen.php
+++ crmeb/listens/DividendTaskListen.php
@@ -18,10 +18,11 @@ class DividendTaskListen  extends TimerService implements ListenerInterface
         // $dividendPoolService->calculateAndDistributeDividend();
         // 使用新的奖池方式
         $this->tick(1000 * 60 * 70, function () {
-            /** @BonusOfflineService $bonusOfflineService */
+            /** @var BonusOfflineService $bonusOfflineService **/
             $bonusOfflineService = app()->make(BonusOfflineService::class);
             try {
-                $bonusOfflineService->calculateBonus();
+                $info=$bonusOfflineService->calculateBonus();
+                record_log('时间: ' . date('Y-m-d H:i:s') . ', 系统分红: ' . json_encode($info, JSON_UNESCAPED_UNICODE), 'red');
             } catch (\Exception $e) {
                 Log::info('自动检测奖池分红失败' . date('Y-m-d H:i:s', time()));
             }
