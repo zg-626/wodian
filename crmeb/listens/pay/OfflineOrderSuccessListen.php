2647a85dc6584ff0e8a3b8f21a54d724d6768286
lsm <1058474495@qq.com>
2025-05-10 15:59:30 +0800

    style(master): 去除无用注释代码

--- crmeb/listens/pay/OfflineOrderSuccessListen.php
+++ crmeb/listens/pay/OfflineOrderSuccessListen.php
@@ -11,16 +11,6 @@ class OfflineOrderSuccessListen
 {
     public function handle($data): void
     {
-        $orderSn = $data['order_sn'];
-        $is_combine = $data['is_combine'] ?? 0;
-        //$groupOrder = app()->make(StoreGroupOrderRepository::class)->getWhere(['group_order_sn' => $orderSn]);
-        //if (!$groupOrder || $groupOrder->paid == 1) return;
-        /*$orders = [];
-        if ($is_combine) {
-            foreach ($data['data']['sub_orders'] as $order) {
-                $orders[$order['out_trade_no']] = $order;
-            }
-        }*/
         Log::info('微信服务商支付成功回调执行队列' . var_export([$data], 1));
         /** @var StoreOrderOfflineRepository $storeOrderOfflineRepository */
         $storeOrderOfflineRepository = app()->make(StoreOrderOfflineRepository::class);
